5bb6320d702763920842bb8328723ea33788c7e7
TechAdi01 <aditipatel1705@gmail.com>
2025-02-01 19:20:19 +0530

    Add files via upload

---
+++ Week1_Basics_ControlStatements/Set1_BasicJava/HelloWorld.java
@@ -0,0 +1,10 @@
+package Week1_Basic;
+
+import java.sql.SQLOutput;
+
+public class HelloWorld {
+    public static  void main(String[] args){
+        System.out.println("Hello World");
+        System.out.println("This is First Program of Java Learning Series");
+    }
+}
